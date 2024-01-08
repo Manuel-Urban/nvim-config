ac0b116b7a5b0e9d1f49ec46067c0761fabb887a
Manuel Urban <manuel@MBP-von-Manuel.fritz.box>
2024-01-08 22:26:25 +0100

    Add apex lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,7 +18,12 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.apex_ls.setup({})
+      lspconfig.apex_ls.setup({
+          apex_jar_path = vim.fn.expand("$HOME/apex-jorje-lsp.jar"),
+          filetypes = { 'cls', 'trigger', 'apex' },
+          apex_enable_semantic_errors = false,
+          apex_enable_completion_statistics = false,
+      })
       lspconfig.tsserver.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
